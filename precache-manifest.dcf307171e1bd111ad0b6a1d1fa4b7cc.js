60650f3165ce4da27d0f03cd6fdeed6441f19e7b
Constantine.P <pkv989@gmail.com>
2020-04-27 01:47:32 +0600

    Updates

---
+++ precache-manifest.dcf307171e1bd111ad0b6a1d1fa4b7cc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed16f3bb7a706332f4d97d23a323cac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5e452ae2d578b45acf",
+    "url": "/static/css/main.8f803e23.chunk.css"
+  },
+  {
+    "revision": "23a8e7f8e8f6a261d346",
+    "url": "/static/js/2.47e6795d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47e6795d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5e452ae2d578b45acf",
+    "url": "/static/js/main.127c6468.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "0270247a13b4cc4b36c0aacf57ade6c4",
+    "url": "/static/media/open-sans-bold.0270247a.ttf"
+  },
+  {
+    "revision": "30ac8529f3a28e8fcf3acc644f8d7c94",
+    "url": "/static/media/open-sans-bold.30ac8529.eot"
+  },
+  {
+    "revision": "602a6da715d3eddf2dc9a65148241903",
+    "url": "/static/media/open-sans-bold.602a6da7.woff"
+  },
+  {
+    "revision": "7572d25734a07ea5f944f576f128215a",
+    "url": "/static/media/open-sans-bold.7572d257.svg"
+  },
+  {
+    "revision": "cbd728bb0047c14298e97591af1e7b9e",
+    "url": "/static/media/open-sans-bold.cbd728bb.woff2"
+  },
+  {
+    "revision": "07d1a425ccbad19329db2f72d12c4233",
+    "url": "/static/media/open-sans-regular.07d1a425.eot"
+  },
+  {
+    "revision": "168418d16b807b8ca190eb6789d52010",
+    "url": "/static/media/open-sans-regular.168418d1.svg"
+  },
+  {
+    "revision": "67e0ecdd8281f64dca1f619b3ae0d32e",
+    "url": "/static/media/open-sans-regular.67e0ecdd.woff"
+  },
+  {
+    "revision": "9666411f6532dc9c31920ea6059b9728",
+    "url": "/static/media/open-sans-regular.9666411f.woff2"
+  },
+  {
+    "revision": "cb8743d4be18cc8b2229e7081ffc0a44",
+    "url": "/static/media/open-sans-regular.cb8743d4.ttf"
+  }
+]);
